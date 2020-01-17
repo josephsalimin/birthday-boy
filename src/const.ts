621e269e62e2df5f827fe4786a8cae4b745dfd12
Joseph <joseph_salimin@hotmail.com>
2020-01-18 00:03:03 +0700

    Removing enter in const

--- src/const.ts
+++ src/const.ts
@@ -1,12 +1,8 @@
 
 export const REPLY_MESSAGE = {
-  NOT_REGISTERED: `
-    Hi! We've detected that you are currently not registered in our server.\nPlease provide use first with your first name! Thank you!
-  `,
+  NOT_REGISTERED: `Hi! We've detected that you are currently not registered in our server.\nPlease provide use first with your first name! Thank you!`,
   INVALID_NAME: `Can you please restate your name? Thank you!`,
-  NAME_ADDED: `
-    Hello %s! It is very nice to know you! For further registration, can you provide me your birthday (YYYY-MM-DD format)? Thank you!
-  `,
+  NAME_ADDED: `Hello %s! It is very nice to know you! For further registration, can you provide me your birthday (YYYY-MM-DD format)? Thank you!`,
   INVALID_DATE: `Please input date with YYYY-MM-DD format! Thank you :)`,
   VALID_DATE: `Thank you for your input! Do you want to know how many days till your next birthday?`,
   ASK_NEXT_BIRTHDAY: `Hi %s! Do you want to know how many days till your next birthday?`,
